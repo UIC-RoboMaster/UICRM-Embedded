40aa8142b8c62cc8ea5bf526ada7e7639ffcd646
Yang <76504564+yry0008@users.noreply.github.com>
2023-04-15 10:54:03 +0800

    Fix Default Mode

--- programs/Fortress/src/remote_task.cpp
+++ programs/Fortress/src/remote_task.cpp
@@ -1,8 +1,8 @@
 #include "remote_task.h"
 
 remote::DBUS* dbus = nullptr;
-RemoteMode remote_mode = REMOTE_MODE_ADVANCED;
-RemoteMode last_remote_mode = REMOTE_MODE_ADVANCED;
+RemoteMode remote_mode = REMOTE_MODE_MANUAL;
+RemoteMode last_remote_mode = REMOTE_MODE_MANUAL;
 RemoteMode available_remote_mode[] = {REMOTE_MODE_MANUAL, REMOTE_MODE_SPIN, REMOTE_MODE_ADVANCED};
 const int8_t remote_mode_max = 3;
 const int8_t remote_mode_min = 1;
