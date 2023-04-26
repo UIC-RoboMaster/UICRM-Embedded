28a67e67036911962dfb0791a88920a93b04b658
Yang <yangruoyu000@live.com>
2023-04-26 21:09:32 +0800

    Add PID for flywheel motor

---
+++ examples/DJI_Board_TypeC_general/motor/m3508_flywheel.cpp
@@ -0,0 +1,60 @@
+#include "bsp_gpio.h"
+#include "bsp_print.h"
+#include "cmsis_os.h"
+#include "main.h"
+#include "motor.h"
+
+#define KEY_GPIO_GROUP KEY_GPIO_Port
+#define KEY_GPIO_PIN KEY_Pin
+
+// Refer to typeC datasheet for channel detail
+static bsp::CAN* can1 = nullptr;
+static control::Motor3508* motor1 = nullptr;
+static float* pid_param = nullptr;
+static control::FlyWheelMotor* flywheel = nullptr;
+
+void RM_RTOS_Init() {
+    print_use_uart(&huart6);
+    can1 = new bsp::CAN(&hcan1, 0x201, true);
+    motor1 = new control::Motor3508(can1, 0x201);
+    pid_param = new float[3]{150, 1, 0.15};
+    control::flywheel_t flywheel_data = {
+        .motor = motor1,
+        .max_speed = 400 * PI,
+        .omega_pid_param = pid_param,
+        .is_inverted = false,
+    };
+    flywheel = new control::FlyWheelMotor(flywheel_data);
+    // Snail need to be run at idle throttle for some
+    HAL_Delay(1000);
+}
+
+void RM_RTOS_Default_Task(const void* args) {
+    UNUSED(args);
+    bsp::GPIO key(KEY_GPIO_GROUP, KEY_GPIO_PIN);
+    control::MotorCANBase* motors[] = {motor1};
+    float current = 0;
+    while (true) {
+        set_cursor(0, 0);
+        clear_screen();
+        if (key.Read() == 0) {
+            osDelay(30);
+            if (key.Read() == 1)
+                continue;
+            while (key.Read() == 0) {
+                osDelay(30);
+            }
+            if (current == 0) {
+                current = 200*PI;
+                flywheel->SetSpeed(current);
+            } else {
+                current = 0;
+                flywheel->SetSpeed(current);
+            }
+        }
+        flywheel->CalcOutput();
+        control::MotorCANBase::TransmitOutput(motors, 1);
+        flywheel->PrintData();
+        osDelay(1);
+    }
+}
