6f79ad997f01c5685f4614706f1ffee82bdcfeae
Yang <yangruoyu000@live.com>
2023-11-08 00:12:35 +0800

    Update motor.h

--- boards/drivers/include/motor.h
+++ boards/drivers/include/motor.h
@@ -676,6 +676,12 @@ namespace driver {
         float GetOmegaDelta(const float target) const;
 
         /**
+         * @brief 更新电机的反馈数据
+         * @note 仅在CAN回调函数中使用，不要在其他地方调用
+         *
+         * @param data[]  原始数据
+         */
+        /**
          * @brief update the current theta for the servomotor
          * @note only used in CAN callback, do not call elsewhere
          *
@@ -735,6 +741,9 @@ namespace driver {
     typedef bool (*align_detect_t)(void);
 
     /**
+     * @brief 舵轮用转向电机的初始化结构体
+     */
+    /**
      * @brief structure used when steering motor instance is initialized
      */
     typedef struct {
@@ -751,6 +760,11 @@ namespace driver {
         float calibrate_offset;
     } steering_t;
 
+    /**
+     * @brief 舵轮用转向电机的包装类，用于精确控制电机的角度，可以用于带有外部减速箱的电机
+     *
+     * @note 这个类现在还处于测试阶段，如果没有专业知识，请不要使用
+     */
     class SteeringMotor {
       public:
         SteeringMotor(steering_t data);
@@ -776,17 +790,54 @@ namespace driver {
         bool align_complete_;
     };
 
+    /**
+     * @brief 达妙4310电机的操作模式
+     *
+     * @note 电机的操作模式决定了电机的输出参数
+     *
+     * @enum MIT                  电机的输出参数为角度与扭矩，此模式仅限有经验的用户使用
+     * @enum POS_VEL              电机的输出参数为位置和速度，电机的输出为位置和速度控制
+     * @enum VEL                  电机的输出参数为速度，电机的输出为速度控制
+     */
+     /**
+      * @brief operation mode of DM m4310 motor
+      *
+      * @note operation mode of motor determines the output parameters of motor
+      *
+      * @enum MIT                  output parameters are angle and torque, only for experienced users
+      * @enum POS_VEL              output parameters are position and velocity, output is position and velocity control
+      * @enum VEL                  output parameters are velocity, output is velocity control
+      */
     typedef enum {
         MIT = 0,
         POS_VEL = 1,
         VEL = 2,
     } m4310_mode_t;
+
     /**
-     * @brief m4310 motor class
+     * @brief 达妙4310电机的标准类
+     *
+     * @note 达妙电机使用CAN通信，但是达妙电机的CAN通信协议与DJI的不同，因此需要单独实现
+     */
+    /**
+     * @brief DM m4310 motor class
+     *
+     * @note DM motor uses CAN communication, but the CAN protocol is different from DJI's, so it needs to be implemented separately
      */
     class Motor4310 {
       public:
-        /** constructor wrapper over MotorCANBase
+        /**
+         * @brief 基础构造函数
+         * @note 这个构造函数与MotorCANBase的构造函数不同。
+         *      4310电机的CAN id与实际CAN id不同，因此需要单独指定
+         *      不同模式下的CAN id如下：
+         *      MIT:                  实际CAN id
+         *      角度速度模式:    实际CAN id + 0x100
+         *      纯速度模式:             实际CAN id + 0x200
+         */
+        /**
+         *  @brief base constructor
+         *  @note constructor wrapper is different from MotorCANBase
          *  CAN frame id for different modes:
          *      MIT:                  actual CAN id.
          *      position-velocity:    CAN id + 0x100.
@@ -800,22 +851,61 @@ namespace driver {
          */
         Motor4310(bsp::CAN* can, uint16_t rx_id, uint16_t tx_id, m4310_mode_t mode);
 
-        /* implements data update callback */
+        /**
+         * @brief 更新电机的反馈数据
+         *
+         * @note 仅在CAN回调函数中使用，不要在其他地方调用
+         *
+         * @param data 原始数据
+         */
+         /**
+          * @brief update the current theta for the motor
+          *
+          * @note only used in CAN callback, do not call elsewhere
+          *
+          * @param data raw data bytes
+          */
         void UpdateData(const uint8_t data[]);
 
-        /* enable m4310 */
+        /**
+         * @brief 使能4310
+         */
+         /**
+          * @brief enable 4310
+          */
         void MotorEnable();
-        /* disable m4310 */
+        /**
+         * @brief 失能4310
+         */
+         /**
+          * @brief disable 4310
+          */
         void MotorDisable();
-        /* set zero position */
+        /**
+         * @brief 更新电机的零点
+         */
+        /**
+         * @brief update zero position of motor
+         */
         void SetZeroPos();
 
+        /**
+         * @brief 获取电机的角度，单位为[rad]
+         *
+         * @return 电机的弧度角度
+         */
         /**
          * @brief get motor angle, in [rad]
          *
          * @return radian angle
          */
         float GetTheta() const;
+
+        /**
+         * @brief 获取电机的角速度，单位为[rad / s]
+         *
+         * @return 电机的弧度角速度
+         */
         /**
          * @brief get motor angular velocity, in [rad / s]
          * @return radian angular velocity
@@ -823,32 +913,92 @@ namespace driver {
         float GetOmega() const;
 
         /**
+         * @brief 获取电机的扭矩，单位为[Nm]
+         *
+         * @return 电机的扭矩
+         */
+        /**
          * @brief get motor torque, in [Nm]
          * @return motor torque
          */
         float GetTorque() const;
+
         /**
-         * implements transmit output specifically for 4310
-         * @param motor m4310 motor object
-         * @param mode operation modes:
-         *                0: MIT
-         *                1: position-velocity
-         *                2: velocity
+         * @brief 直接传输数据到电机
+         *
+         * @note 由于一个4310电机需要使用一个独立的CAN id，因此这个函数不是静态的，直接由对象调用
+         */
+        /**
+         * @brief transmit data to motor
+         *
+         * @note since a 4310 motor needs to use a separate CAN id, this function is not static, directly called by the object
          */
         void TransmitOutput();
 
-        /* implements data printout */
+        /**
+         * @brief 打印电机数据
+         */
+         /**
+          * @brief print out motor data
+          */
         void PrintData();
 
-        /* set output parameters for m4310 using MIT mode */
+        /**
+         * @brief 设置电机的输出参数
+         * @note 本调用方式推荐用于MIT模式下，其他模式下请使用其他重载函数
+         * @param position 电机的角度，单位为[rad]
+         * @param velocity 电机的角速度，单位为[rad / s]
+         * @param kp 电机的kp值，仅在MIT模式下有效
+         * @param kd 电机的kd值，仅在MIT模式下有效
+         * @param torque 电机的扭矩，仅在MIT模式下有效
+         */
+         /**
+          * @brief set output parameters for m4310
+          * @note this function is recommended for MIT mode, use other overloaded functions for other modes
+          * @param position motor angle, in [rad]
+          * @param velocity motor angular velocity, in [rad / s]
+          * @param kp kp value of motor, only valid in MIT mode
+          * @param kd kd value of motor, only valid in MIT mode
+          * @param torque motor torque, only valid in MIT mode
+          */
         void SetOutput(float position, float velocity, float kp, float kd, float torque);
 
-        /* set output parameters for m4310 using position-velocity mode */
+        /**
+         * @brief 设置电机的输出参数
+         * @note 本调用方式推荐用于角度速度模式下，其他模式下请使用其他重载函数
+         * @param position 电机的角度，单位为[rad]
+         * @param velocity 电机的角速度，单位为[rad / s]
+         */
+         /**
+          * @brief set output parameters for m4310 using position-velocity mode
+          * @note this function is recommended for position-velocity mode, use other overloaded functions for other modes
+          * @param position motor angle, in [rad]
+          * @param velocity motor angular velocity, in [rad / s]
+          */
         void SetOutput(float position, float velocity);
 
-        /* set output parameters for m4310 using velocity mode */
+        /**
+         * @brief 设置电机的输出参数
+         * @note 本调用方式推荐用于纯速度模式下，其他模式下请使用其他重载函数
+         * @param velocity 电机的角速度，单位为[rad / s]
+         */
+         /**
+          * @brief set output parameters for m4310 using velocity mode
+          * @note this function is recommended for velocity mode, use other overloaded functions for other modes
+          * @param velocity motor angular velocity, in [rad / s]
+          */
         void SetOutput(float velocity);
 
+        /**
+         * @brief 转换函数，将float转换为unsigned int，给定范围和位数；
+         *     详见m4310 V1.2文档
+         *
+         *     @param x 要转换的值
+         *     @param x_min 当前参数的最小值
+         *     @param x_max 当前参数的最大值
+         *     @param bits 位数
+         *     @return 从float转换为unsigned int的值
+         */
         /**
          * @brief Converts a float to an unsigned int, given range and number of bits;
          *      see m4310 V1.2 document for detail
@@ -860,13 +1010,22 @@ namespace driver {
          */
         static uint16_t float_to_uint(float x, float x_min, float x_max, int bits);
         /**
+         * @brief 转换函数，将unsigned int转换为float，给定范围和位数；
+         *    详见m4310 V1.2文档
+         * @param x_int 要转换的值
+         * @param x_min 当前参数的最小值
+         * @param x_max 当前参数的最大值
+         * @param bits 位数
+         * @return 从unsigned int转换为float的值
+         */
+        /**
          * @brief Converts an unsigned int to a float, given range and number of bits;
          *      see m4310 V1.2 document for detail
          * @param x_int value to be converted
          * @param x_min minimum value of the current parameter
          * @param x_max maximum value of the current parameter
          * @param bits size in bits
-         * @return value converted from float to unsigned int
+         * @return value converted from unsigned int to float
          */
         static float uint_to_float(int x_int, float x_min, float x_max, int bits);
 
@@ -907,7 +1066,12 @@ namespace driver {
     };
 
     /**
+     * @brief 飞轮电机的初始化结构体
+     *
+     */
+    /**
      * @brief structure used when flywheel motor instance is initialized
+     *
      */
     typedef struct {
         MotorCANBase* motor;    /* motor instance to be wrapped as a flywheel      */
@@ -917,34 +1081,73 @@ namespace driver {
     } flywheel_t;
 
     /**
+     * @brief 飞轮电机的包装类
+     *
+     *
+     * @note 飞轮电机是一个特殊的类，使用PID控制器控制电机的角速度，使速度达到目标值且恒定
+     * 通常用来控制摩擦轮的转速
+     */
+    /**
      * @brief class for flywheel motor
+     *
+     * @note flywheel motor is a special class that uses PID controller to control the angular velocity of motor, making the velocity reach the target and constant
+     * usually used to control the speed of friction wheel
      */
     class FlyWheelMotor {
       public:
+        /**
+         * @brief 基础构造函数
+         * @param data 飞轮电机的初始化结构体，参考flywheel_t
+         */
         /**
          * @brief constructor for flywheel motor
          * @param data the data for flywheel
          */
         FlyWheelMotor(flywheel_t data);
         /**
+         * @brief 设置电机的旋转速度
+         * @param speed 电机的目标旋转速度，单位为[rad/s]
+         */
+        /**
          * @brief set the speed of flywheel motor
          * @param speed the target speed of flywheel motor in [rad/s]
          */
         void SetSpeed(float speed);
         /**
+         * @brief 计算电机的输出
+         *
+         * @note 这个函数不会直接控制电机，它只会计算电机的输出值
+         */
+        /**
          * @brief calculate the output of flywheel motor
+         *
+         * @note this function will not command the motor, it only calculate the desired input
          */
         void CalcOutput();
         /**
+         * @brief 获取电机的目标速度，单位为[rad / s]
+         *
+         * @return 电机的目标速度
+         */
+        /**
          * @brief get current target for flywheel, in [rad/s]
          *
+         * @return current target speed
          */
         float GetTarget() const;
+        /**
+         * @brief 打印电机数据
+         */
         /**
          * @brief print out motor data
          */
         void PrintData() const;
 
+        /**
+         * @brief 获取电机的角度，单位为[rad]
+         *
+         * @return 电机的弧度角度，范围为[0, 2PI]
+         */
         /**
          * @brief get motor angle, in [rad]
          *
@@ -952,6 +1155,13 @@ namespace driver {
          */
         float GetTheta() const;
 
+        /**
+         * @brief 获取电机的角度与目标角度的角度差，单位为[rad]
+         *
+         * @param target 目标角度，单位为[rad]
+         *
+         * @return 与目标角度的弧度角度差，范围为[-PI, PI]
+         */
         /**
          * @brief get angle difference (target - actual), in [rad]
          *
@@ -962,12 +1172,24 @@ namespace driver {
         float GetThetaDelta(const float target) const;
 
         /**
+         * @brief 获取电机的角速度，单位为[rad / s]
+         *
+         * @return 电机的角速度
+         */
+        /**
          * @brief get angular velocity, in [rad / s]
          *
          * @return angular velocity
          */
         float GetOmega() const;
 
+        /**
+         * @brief 获取电机的角速度与目标角速度的角速度差，单位为[rad / s]
+         *
+         * @param target 目标角速度，单位为[rad / s]
+         *
+         * @return 与目标角速度的角速度差
+         */
         /**
          * @brief get angular velocity difference (target - actual), in [rad / s]
          *
@@ -977,6 +1199,13 @@ namespace driver {
          */
         float GetOmegaDelta(const float target) const;
 
+        /**
+         * @brief 更新电机的反馈数据
+         *
+         * @note 仅在CAN回调函数中使用，不要在其他地方调用
+         *
+         * @param data[]  原始数据
+         */
         /**
          * @brief update the current theta for the servomotor
          * @note only used in CAN callback, do not call elsewhere
