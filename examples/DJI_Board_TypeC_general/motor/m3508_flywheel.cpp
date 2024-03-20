5685f9fc0db0a069b4d129932ce21d5db08b0b80
Yang <yry0008@icloud.com>
2024-03-20 13:00:39 +0800

    Add M3508 Dart

--- examples/DJI_Board_TypeC_general/motor/m3508_flywheel.cpp
+++ examples/DJI_Board_TypeC_general/motor/m3508_flywheel.cpp
@@ -32,25 +32,31 @@
 static bsp::CAN* can2 = nullptr;
 static driver::Motor3508* motor1 = nullptr;
 static driver::Motor3508* motor2 = nullptr;
+static driver::Motor3508* motor3 = nullptr;
+static driver::Motor3508* motor4 = nullptr;
 
 void RM_RTOS_Init() {
-    print_use_uart(&huart1);
-    can2 = new bsp::CAN(&hcan2, false);
+    print_use_uart(&huart1,true,3000000);
+    can2 = new bsp::CAN(&hcan1, true);
     motor1 = new driver::Motor3508(can2, 0x201);
     motor2 = new driver::Motor3508(can2, 0x202);
+    motor3 = new driver::Motor3508(can2, 0x203);
+    motor4 = new driver::Motor3508(can2, 0x204);
 
     motor1->SetTransmissionRatio(1);
     motor2->SetTransmissionRatio(1);
+    motor3->SetTransmissionRatio(1);
+    motor4->SetTransmissionRatio(1);
 
     control::ConstrainedPID::PID_Init_t omega_pid_init = {
-        .kp = 500,
-        .ki = 3,
+        .kp = 250,
+        .ki = 0.3,
         .kd = 0,
         .max_out = 30000,
         .max_iout = 10000,
         .deadband = 0,                          // 死区
-        .A = 3 * PI,                            // 变速积分所能达到的最大值为A+B
-        .B = 2 * PI,                            // 启动变速积分的死区
+        .A = 30 * PI,                            // 变速积分所能达到的最大值为A+B
+        .B = 20 * PI,                            // 启动变速积分的死区
         .output_filtering_coefficient = 0.1,    // 输出滤波系数
         .derivative_filtering_coefficient = 0,  // 微分滤波系数
         .mode = control::ConstrainedPID::Integral_Limit |       // 积分限幅
@@ -60,8 +66,12 @@ void RM_RTOS_Init() {
     };
     motor1->ReInitPID(omega_pid_init, driver::MotorCANBase::OMEGA);
     motor2->ReInitPID(omega_pid_init, driver::MotorCANBase::OMEGA);
-    motor1->SetMode(driver::MotorCANBase::OMEGA);
-    motor2->SetMode(driver::MotorCANBase::OMEGA | driver::MotorCANBase::INVERTED);
+    motor3->ReInitPID(omega_pid_init, driver::MotorCANBase::OMEGA);
+    motor4->ReInitPID(omega_pid_init, driver::MotorCANBase::OMEGA);
+    motor1->SetMode(driver::MotorCANBase::OMEGA|driver::MotorCANBase::INVERTED);
+    motor2->SetMode(driver::MotorCANBase::OMEGA);
+    motor3->SetMode(driver::MotorCANBase::OMEGA| driver::MotorCANBase::INVERTED);
+    motor4->SetMode(driver::MotorCANBase::OMEGA );
 
     // Snail need to be run at idle throttle for some
     HAL_Delay(1000);
@@ -72,8 +82,8 @@ void RM_RTOS_Default_Task(const void* args) {
     bsp::GPIO key(KEY_GPIO_GROUP, KEY_GPIO_PIN);
     int current = 0;
     while (true) {
-        set_cursor(0, 0);
-        clear_screen();
+//        set_cursor(0, 0);
+//        clear_screen();
         if (key.Read() == 0) {
             osDelay(30);
             if (key.Read() == 1)
@@ -83,19 +93,24 @@ void RM_RTOS_Default_Task(const void* args) {
             }
             if (current == 0) {
                 current = 10000;
-                motor1->SetTarget(80 * 2 * PI);
-                motor2->SetTarget(80 * 2 * PI);
+                motor1->SetTarget(120 * 2 * PI);
+                motor2->SetTarget(120 * 2 * PI);
+                motor3->SetTarget(120 * 2 * PI);
+                motor4->SetTarget(120 * 2 * PI);
             } else {
                 current = 0;
                 motor1->SetTarget(0);
                 motor2->SetTarget(0);
+                motor3->SetTarget(0);
+                motor4->SetTarget(0);
             }
 
             osDelay(20);
         }
-        motor1->PrintData();
-        motor2->PrintData();
+        print("%.2f %.2f %.2f %.2f\r\n", motor1->GetOmega(), motor2->GetOmega(), motor3->GetOmega(),
+              motor4->GetOmega());
 
-        osDelay(20);
+
+        osDelay(1);
     }
 }
