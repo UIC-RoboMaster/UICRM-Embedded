03f70fe2ff360eba3ba8c7045e9a73dd0fcdb488
Sequen4e <gTTgFrank@foxmail.com>
2024-12-01 02:23:59 +0800

    Changes

---
+++ programs/Neo/include/remote_task.h
@@ -0,0 +1,73 @@
+/*###########################################################
+ # Copyright (c) 2023-2024. BNU-HKBU UIC RoboMaster         #
+ #                                                          #
+ # This program is free software: you can redistribute it   #
+ # and/or modify it under the terms of the GNU General      #
+ # Public License as published by the Free Software         #
+ # Foundation, either version 3 of the License, or (at      #
+ # your option) any later version.                          #
+ #                                                          #
+ # This program is distributed in the hope that it will be  #
+ # useful, but WITHOUT ANY WARRANTY; without even           #
+ # the implied warranty of MERCHANTABILITY or FITNESS       #
+ # FOR A PARTICULAR PURPOSE.  See the GNU General           #
+ # Public License for more details.                         #
+ #                                                          #
+ # You should have received a copy of the GNU General       #
+ # Public License along with this program.  If not, see     #
+ # <https://www.gnu.org/licenses/>.                         #
+ ###########################################################*/
+
+#pragma once
+#include "cmsis_os2.h"
+#include "dbus.h"
+#include "main.h"
+#include "referee_task.h"
+#include "user_define.h"
+#include "utils.h"
+extern remote::DBUS* dbus;
+extern bool turbo_shoot;
+/**
+ * @brief Dbus Init
+ */
+void init_dbus();
+
+enum RemoteMode {
+    REMOTE_MODE_PREPARE = -2,
+    REMOTE_MODE_KILL = -1,
+    REMOTE_MODE_STOP = 0,
+    REMOTE_MODE_FOLLOW = 1,
+    REMOTE_MODE_SPIN = 2,
+    REMOTE_MODE_ADVANCED = 3
+};
+extern RemoteMode remote_mode;
+enum ShootFricMode {
+    SHOOT_FRIC_MODE_DISABLE = -1,
+    SHOOT_FRIC_MODE_STOP = 0,
+    SHOOT_FRIC_MODE_PREPARING = 1,
+    SHOOT_FRIC_MODE_PREPARED = 2,
+    SHOOT_FRIC_SPEEDUP = 3,
+    SHOOT_FRIC_SPEEDDOWN = 4,
+};
+extern ShootFricMode shoot_flywheel_mode;
+enum ShootMode {
+    SHOOT_MODE_DISABLE = -1,
+    SHOOT_MODE_STOP = 0,
+    SHOOT_MODE_PREPARING = 1,
+    SHOOT_MODE_PREPARED = 2,
+    SHOOT_MODE_SINGLE = 3,
+    SHOOT_MODE_BURST = 4,
+};
+extern ShootMode shoot_load_mode;
+extern osThreadId_t remoteTaskHandle;
+const osThreadAttr_t remoteTaskAttribute = {.name = "remoteTask",
+                                            .attr_bits = osThreadDetached,
+                                            .cb_mem = nullptr,
+                                            .cb_size = 0,
+                                            .stack_mem = nullptr,
+                                            .stack_size = 512 * 4,
+                                            .priority = (osPriority_t)osPriorityHigh7,
+                                            .tz_module = 0,
+                                            .reserved = 0};
+void remoteTask(void* arg);
+void init_remote();
---
+++ programs/Neo/src/main.cpp
@@ -0,0 +1,163 @@
+/*###########################################################
+ # Copyright (c) 2023-2024. BNU-HKBU UIC RoboMaster         #
+ #                                                          #
+ # This program is free software: you can redistribute it   #
+ # and/or modify it under the terms of the GNU General      #
+ # Public License as published by the Free Software         #
+ # Foundation, either version 3 of the License, or (at      #
+ # your option) any later version.                          #
+ #                                                          #
+ # This program is distributed in the hope that it will be  #
+ # useful, but WITHOUT ANY WARRANTY; without even           #
+ # the implied warranty of MERCHANTABILITY or FITNESS       #
+ # FOR A PARTICULAR PURPOSE.  See the GNU General           #
+ # Public License for more details.                         #
+ #                                                          #
+ # You should have received a copy of the GNU General       #
+ # Public License along with this program.  If not, see     #
+ # <https://www.gnu.org/licenses/>.                         #
+ ###########################################################*/
+
+#include "main.h"
+
+#include "bsp_os.h"
+#include "bsp_print.h"
+#include "buzzer_notes.h"
+#include "buzzer_task.h"
+#include "chassis_task.h"
+#include "cmsis_os.h"
+#include "gimbal_task.h"
+#include "imu_task.h"
+#include "public_port.h"
+// #include "referee_task.h"
+#include "remote_task.h"
+#include "shoot_task.h"
+#include "ui_task.h"
+#include "user_define.h"
+void RM_RTOS_Init(void) {
+    bsp::SetHighresClockTimer(&htim5);
+    print_use_uart(&huart1, true, 921600);
+    init_can();
+    init_batt();
+    init_imu();
+    init_buzzer();
+    init_referee();
+    init_remote();
+    init_shoot();
+    init_gimbal();
+    init_chassis();
+    init_ui();
+}
+
+void RM_RTOS_Threads_Init(void) {
+    imuTaskHandle = osThreadNew(imuTask, nullptr, &imuTaskAttribute);
+    buzzerTaskHandle = osThreadNew(buzzerTask, nullptr, &buzzerTaskAttribute);
+    //    refereeTaskHandle = osThreadNew(refereeTask, nullptr, &refereeTaskAttribute);
+    //    refereercTaskHandle = osThreadNew(refereercTask, nullptr, &refereercTaskAttribute);
+    remoteTaskHandle = osThreadNew(remoteTask, nullptr, &remoteTaskAttribute);
+    gimbalTaskHandle = osThreadNew(gimbalTask, nullptr, &gimbalTaskAttribute);
+    chassisTaskHandle = osThreadNew(chassisTask, nullptr, &chassisTaskAttribute);
+    shootTaskHandle = osThreadNew(shootTask, nullptr, &shootTaskAttribute);
+    if (ENABLE_UI)
+        uiTaskHandle = osThreadNew(uiTask, nullptr, &uiTaskAttribute);
+}
+
+void RM_RTOS_Default_Task(const void* arg) {
+    UNUSED(arg);
+    osDelay(3000);
+    Buzzer_Sing(DJI);
+    //    while (true) {
+    //        clear_screen();
+    //        set_cursor(0, 0);
+    //        flywheel_left->PrintData();
+    //        flywheel_right->PrintData();
+    //        osDelay(10);
+    //    }
+    char s[50];
+    while (true) {
+        set_cursor(0, 0);
+        clear_screen();
+        switch (remote_mode) {
+            case REMOTE_MODE_PREPARE:
+                strcpy(s, "PREPARE");
+                break;
+            case REMOTE_MODE_STOP:
+                strcpy(s, "STOP");
+                break;
+            case REMOTE_MODE_KILL:
+                strcpy(s, "KILL");
+                break;
+            case REMOTE_MODE_FOLLOW:
+                strcpy(s, "MANUAL");
+                break;
+            case REMOTE_MODE_SPIN:
+                strcpy(s, "SPIN");
+                break;
+            default:
+                strcpy(s, "UNKNOWN");
+                break;
+        }
+        print("Mode:%s\r\n", s);
+        //        switch (shoot_fric_mode) {
+        //            case SHOOT_FRIC_MODE_PREPARING:
+        //                strcpy(s, "PREPARE");
+        //                break;
+        //            case SHOOT_FRIC_MODE_STOP:
+        //                strcpy(s, "STOP");
+        //                break;
+        //            case SHOOT_FRIC_MODE_PREPARED:
+        //                strcpy(s, "PREPARED");
+        //                break;
+        //            case SHOOT_FRIC_MODE_DISABLE:
+        //                strcpy(s, "DISABLE");
+        //                break;
+        //        }
+        //        print("Shoot Fric Mode:%s\r\n", s);
+        switch (shoot_load_mode) {
+            case SHOOT_MODE_PREPARING:
+                strcpy(s, "PREPARE");
+                break;
+            case SHOOT_MODE_STOP:
+                strcpy(s, "STOP");
+                break;
+            case SHOOT_MODE_PREPARED:
+                strcpy(s, "PREPARED");
+                break;
+            case SHOOT_MODE_DISABLE:
+                strcpy(s, "DISABLE");
+                break;
+            case SHOOT_MODE_SINGLE:
+                strcpy(s, "SINGLE");
+                break;
+            case SHOOT_MODE_BURST:
+                strcpy(s, "BURST");
+                break;
+        }
+        print("Shoot Mode:%s\r\n", s);
+
+        print("# %.2f s, IMU %s\r\n", HAL_GetTick() / 1000.0,
+              imu->DataReady() ? "\033[1;42mReady\033[0m" : "\033[1;41mNot Ready\033[0m");
+        print("Temp: %.2f\r\n", imu->Temp);
+        print("Heater: %.2f\r\n", imu->TempPWM);
+        print("Euler Angles: %.2f, %.2f, %.2f\r\n", imu->INS_angle[0] / PI * 180,
+              imu->INS_angle[1] / PI * 180, imu->INS_angle[2] / PI * 180);
+        print("Is Calibrated: %s\r\n",
+              imu->CaliDone() ? "\033[1;42mYes\033[0m" : "\033[1;41mNo\033[0m");
+        print("Yaw Motor: %.2f, %.2f\r\n", yaw_motor->GetTheta(), yaw_motor->GetOmega());
+        print("Pitch Motor: %.2f, %.2f\r\n", pitch_motor->GetTheta(), pitch_motor->GetOmega());
+        print("Chassis Volt: %.3f\r\n", referee->power_heat_data.chassis_volt / 1000.0);
+        print("Chassis Curr: %.3f\r\n", referee->power_heat_data.chassis_current / 1000.0);
+        print("Chassis Power: %.3f\r\n", referee->power_heat_data.chassis_power);
+        print("\r\n");
+        print("Shooter Cooling Heat: %hu\r\n",
+              referee->power_heat_data.shooter_id1_42mm_cooling_heat);
+        print("Bullet Frequency: %hhu\r\n", referee->shoot_data.bullet_freq);
+        print("Bullet Speed: %.3f\r\n", referee->shoot_data.bullet_speed);
+        print("\r\n");
+        print("Current HP %d/%d\n", referee->game_robot_status.remain_HP,
+              referee->game_robot_status.max_HP);
+        print("Remain bullet %d\n", referee->bullet_remaining.bullet_remaining_num_42mm);
+        print("clear\n");
+        osDelay(75);
+    }
+}
