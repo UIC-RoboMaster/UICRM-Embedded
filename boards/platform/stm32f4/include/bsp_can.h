a1c8ef0d176707610a6423cb6171d45d475edc8b
jyt <jytpeterjiang@gmail.com>
2025-06-10 17:07:33 +0800

    add adernal_supercap

--- boards/platform/stm32f4/include/bsp_can.h
+++ boards/platform/stm32f4/include/bsp_can.h
@@ -192,6 +192,8 @@ namespace bsp {
          * @note should not be called explicitly form the application side
          */
         void RxExtendCallback(CAN_RxHeaderTypeDef header, uint8_t* data);
+        // 将FindInstance从private改到public以便访问
+        static CAN* FindInstance(CAN_HandleTypeDef* hcan);
 
       private:
         void ConfigureFilter(bool is_master);
@@ -215,7 +217,6 @@ namespace bsp {
         uint32_t id_in_tx_mailbox_[3] = {0};
 
         static std::unordered_map<CAN_HandleTypeDef*, CAN*> ptr_map;
-        static CAN* FindInstance(CAN_HandleTypeDef* hcan);
         static bool HandleExists(CAN_HandleTypeDef* hcan);
         static void RxFIFO0MessagePendingCallback(CAN_HandleTypeDef* hcan);
     };
