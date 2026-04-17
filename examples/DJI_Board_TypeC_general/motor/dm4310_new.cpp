c517cce8607d06320e9bd9a1ea369e3af9d94654
Martin <wzr2024bb@gmail.com>
2026-04-17 21:56:35 +0800

    add: 使用DMmotor重写dm4310 example

--- examples/DJI_Board_TypeC_general/motor/dm4310_new.cpp
+++ examples/DJI_Board_TypeC_general/motor/dm4310_new.cpp
@@ -18,52 +18,38 @@
 # <https://www.gnu.org/licenses/>.                         #
 ###########################################################*/
 
-#include "MotorCanBase.h"
+#include "DMmotor.h"
 #include "bsp_gpio.h"
+#include "bsp_os.h"
 #include "bsp_print.h"
 #include "cmsis_os.h"
 #include "main.h"
-#include "pid.h"
 
 #define KEY_GPIO_GROUP KEY_GPIO_Port
 #define KEY_GPIO_PIN KEY_Pin
 
-// Refer to typeA datasheet for channel detail
 static bsp::CAN* can1 = nullptr;
-static driver::MotorDM4310* motor1 = nullptr;
+static driver::DMMotor4310* motor1 = nullptr;
 
 void RM_RTOS_Init() {
+    bsp::SetHighresClockTimer(&BOARD_TIM_SYS);
     print_use_uart(&huart1);
+
     can1 = new bsp::CAN(&hcan1, true);
-    motor1 = new driver::MotorDM4310(can1, 0x009, 0x001);
-    motor1->SetTransmissionRatio(10);
-    control::ConstrainedPID::PID_Init_t omega_pid_init = {
-        .kp = 2,
-        .ki = 0,
-        .kd = 0.1,
-        .max_out = 100,
-        .max_iout = 300,
-        .deadband = 0,                          // 死区
-        .A = 1.5 * PI,                          // 变速积分所能达到的最大值为A+B
-        .B = 1 * PI,                            // 启动变速积分的死区
-        .output_filtering_coefficient = 0.1,    // 输出滤波系数
-        .derivative_filtering_coefficient = 0,  // 微分滤波系数
-        .mode = control::ConstrainedPID::Integral_Limit |       // 积分限幅
-                control::ConstrainedPID::OutputFilter |         // 输出滤波
-                control::ConstrainedPID::Trapezoid_Intergral |  // 梯形积分
-                control::ConstrainedPID::ChangingIntegralRate,  // 变速积分
-    };
-    motor1->ReInitPID(omega_pid_init, driver::MotorCANBase::OMEGA);
-    motor1->SetMode(driver::MotorCANBase::OMEGA | driver::MotorCANBase::ABSOLUTE);
-    motor1->SetTarget(0);
-    // Snail need to be run at idle throttle for some
+    motor1 = new driver::DMMotor4310(can1, 0x009, 0x001, driver::VEL);
+
+    // DM4310 needs explicit enable command before accepting runtime control frames.
+    motor1->MotorEnable();
+    HAL_Delay(100);
+    motor1->SetOutput(0.0f);
+    motor1->TransmitOutput();
     HAL_Delay(1000);
 }
 
 void RM_RTOS_Default_Task(const void* args) {
     UNUSED(args);
     bsp::GPIO key(KEY_GPIO_GROUP, KEY_GPIO_PIN);
-    int current = 0;
+
     while (true) {
         set_cursor(0, 0);
         clear_screen();
@@ -72,17 +58,12 @@ void RM_RTOS_Default_Task(const void* args) {
             if (key.Read() == 1)
                 continue;
             while (key.Read() == 0) {
+                motor1->SetOutput(20);
+                motor1->TransmitOutput();  // 发送CAN控制帧
                 osDelay(30);
             }
-            if (current == 0) {
-                current = 10000;
-                motor1->SetTarget(2 * PI);
-            } else {
-                current = 0;
-                motor1->SetTarget(0);
-            }
-
-            osDelay(20);
+            motor1->SetOutput(0);
+            motor1->TransmitOutput();  // 停止电机
         }
         motor1->PrintData();
         osDelay(20);
