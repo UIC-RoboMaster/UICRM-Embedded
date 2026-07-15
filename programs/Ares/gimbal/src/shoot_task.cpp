4319ab77dcac15246a7ff02c2fd0486bf3e8c7d8
Martin <m2rt1n88@gmail.com>
2026-07-15 11:59:07 +0800

    refactor: 对 program/Ares 修正适配新电机类

--- programs/Ares/gimbal/src/shoot_task.cpp
+++ programs/Ares/gimbal/src/shoot_task.cpp
@@ -199,11 +199,11 @@ void init_shoot() {
                 control::ConstrainedPID::Trapezoid_Intergral |  // 梯形积分
                 control::ConstrainedPID::ChangingIntegralRate,  // 变速积分
     };
-    flywheel_left->ReInitPID(omega_pid_init, driver::MotorCANBase::OMEGA);
-    flywheel_right->ReInitPID(omega_pid_init, driver::MotorCANBase::OMEGA);
+    flywheel_left->ReInitPID(omega_pid_init, driver::DjiMotorBase::OMEGA);
+    flywheel_right->ReInitPID(omega_pid_init, driver::DjiMotorBase::OMEGA);
 
-    flywheel_left->SetMode(driver::MotorCANBase::OMEGA | driver::MotorCANBase::INVERTED);
-    flywheel_right->SetMode(driver::MotorCANBase::OMEGA);
+    flywheel_left->SetMode(driver::DjiMotorBase::OMEGA | driver::DjiMotorBase::INVERTED);
+    flywheel_right->SetMode(driver::DjiMotorBase::OMEGA);
 
     steering_up = new driver::Motor2006(can2, 0x203);
     steering_up->SetTransmissionRatio(36);
@@ -237,9 +237,9 @@ void init_shoot() {
         .mode = control::ConstrainedPID::OutputFilter  // 输出滤波
 
     };
-    steering_up->ReInitPID(enhance_theta_pid_init, driver::MotorCANBase::THETA);
-    steering_up->ReInitPID(enhance_omega_pid_init, driver::MotorCANBase::OMEGA);
-    steering_up->SetMode(driver::MotorCANBase::THETA | driver::MotorCANBase::OMEGA);
+    steering_up->ReInitPID(enhance_theta_pid_init, driver::DjiMotorBase::THETA);
+    steering_up->ReInitPID(enhance_omega_pid_init, driver::DjiMotorBase::OMEGA);
+    steering_up->SetMode(driver::DjiMotorBase::THETA | driver::DjiMotorBase::OMEGA);
 
     steering_motor = new driver::Motor3508(can1, 0x201);
 
@@ -258,7 +258,7 @@ void init_shoot() {
         .mode = control::ConstrainedPID::OutputFilter  // 输出滤波
 
     };
-    steering_motor->ReInitPID(steering_theta_pid_init, driver::MotorCANBase::THETA);
+    steering_motor->ReInitPID(steering_theta_pid_init, driver::DjiMotorBase::THETA);
 
     control::ConstrainedPID::PID_Init_t steering_omega_pid_init = {
         .kp = 2500,
@@ -277,8 +277,8 @@ void init_shoot() {
                 control::ConstrainedPID::ChangingIntegralRate |  // 变速积分
                 control::ConstrainedPID::ErrorHandle,            // 错误处理
     };
-    steering_motor->ReInitPID(steering_omega_pid_init, driver::MotorCANBase::OMEGA);
-    steering_motor->SetMode(driver::MotorCANBase::THETA | driver::MotorCANBase::OMEGA);
+    steering_motor->ReInitPID(steering_omega_pid_init, driver::DjiMotorBase::OMEGA);
+    steering_motor->SetMode(driver::DjiMotorBase::THETA | driver::DjiMotorBase::OMEGA);
 
     steering_motor->RegisterErrorCallback(jam_callback, steering_motor);
     // laser = new bsp::Laser(&htim3, 3, 1000000);
