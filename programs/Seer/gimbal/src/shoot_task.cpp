f57d5a8894a45092ea0b57b9dcec09fc426c2ac7
Martin <m2rt1n88@gmail.com>
2026-09-22 20:15:57 +0800

    feat: shoot_task

---
+++ programs/Seer/gimbal/src/shoot_task.cpp
@@ -0,0 +1,215 @@
+/*###########################################################
+ # Copyright (c) 2023-2024. BNU-HKBU UIC RoboMaster         #
+ #                                                          #
+ # This program is free software: you can redistribute it   #
+ # and/or modify it under the terms of the GNU General      #
+ # Public License as published by the Free Software         #
+ # Foundation, either version 3 of the License, or (at      #
+ # your option) any later version.                          #
+ #                                                          #
+ # This program is distributed in the hope that it will be  #
+ # useful, but WITHOUT ANY WARRANTY; without even           #
+ # the implied warranty of MERCHANTABILITY or FITNESS       #
+ # FOR A PARTICULAR PURPOSE.  See the GNU General           #
+ # Public License for more details.                         #
+ #                                                          #
+ # You should have received a copy of the GNU General       #
+ # Public License along with this program.  If not, see     #
+ # <https://www.gnu.org/licenses/>.                         #
+ ###########################################################*/
+
+#include "shoot_task.h"
+
+#include "minipc_task.h"
+
+driver::Motor3508* flywheel_left = nullptr;
+driver::Motor3508* flywheel_right = nullptr;
+
+driver::Motor2006* steering_motor = nullptr;
+
+const float singleShotDivider = 3.5f;
+
+bool jam_notify_flags = false;
+
+void jam_callback(void* args) {
+    driver::Motor2006* motor = static_cast<driver::Motor2006*>(args);
+    jam_notify_flags = true;
+    float target = motor->GetTarget();
+    if (target > motor->GetOutputShaftTheta()) {
+        float prev_target = motor->GetTarget() - 2 * PI / singleShotDivider;
+        motor->SetTarget(prev_target);
+    } else {
+        float prev_target = motor->GetTarget() + 2 * PI / singleShotDivider;
+        motor->SetTarget(prev_target);
+    }
+}
+
+osThreadId_t shootTaskHandle;
+
+void shootTask(void* arg) {
+    UNUSED(arg);
+    // 启动等待
+    osDelay(1000);
+    while (!is_activate) {
+        osDelay(SHOOT_OS_DELAY);
+    }
+    // 等待IMU初始化
+    while (!imu->DataReady() || !imu->CaliDone()) {
+        osDelay(SHOOT_OS_DELAY);
+    }
+    //    int last_state = remote::MID;
+    //    int last_state_2 = remote::MID;
+    //    uint8_t shoot_state = 0;
+    //    int shoot_flywheel_offset = 0;
+    //    uint8_t shoot_state_2 = 0;
+    //    uint8_t last_shoot_key = 0;
+    //    uint16_t shoot_time_count = 0;
+    //    uint8_t servo_back = 0;
+    //    bool can_shoot_click = false;
+
+    float flywheel_target = 0.0;
+    while (true) {
+        osDelay(SHOOT_OS_DELAY);
+
+        bool shoot_en = is_activate;
+#ifdef HAS_REFEREE
+        shoot_en = shoot_en && referee->game_robot_status.mains_power_shooter_output;
+#endif
+        if (!shoot_en) {
+            // 死了
+            kill_shoot();
+            osDelay(SHOOT_OS_DELAY);
+            continue;
+        }
+
+        if (!steering_motor->IsEnable()) {
+            steering_motor->Enable();
+        }
+        if (!flywheel_left->IsEnable()) {
+            flywheel_left->Enable();
+        }
+        if (!flywheel_right->IsEnable()) {
+            flywheel_right->Enable();
+        }
+
+        switch (shoot_fric_wheel_mode) {
+            case SHOOT_FRIC_MODE_PREPARING:
+                flywheel_target = 120.0f * 2 * PI;
+                shoot_fric_wheel_mode = SHOOT_FRIC_MODE_PREPARED;  // todo move to remote_task and
+                                                                   //  trigger by speed of flywheels
+                break;
+            case SHOOT_FRIC_MODE_PREPARED:
+                break;
+            case SHOOT_FRIC_MODE_STOP:
+                flywheel_target = 0.0;
+                break;
+            default:
+                flywheel_target = 0.0;
+                break;
+        }
+        flywheel_left->SetTarget(flywheel_target);
+        flywheel_right->SetTarget(flywheel_target);
+
+        switch (shoot_mode) {
+            case SHOOT_MODE_PREPARING:
+            case SHOOT_MODE_PREPARED:
+                // 准备就绪，未发射状态
+                // 如果检测到未上膛（刚发射一枚子弹），则回到准备模式
+                if (last_shoot_mode == SHOOT_MODE_BURST)
+                    steering_motor->Hold(true);
+                break;
+            case SHOOT_MODE_SINGLE:
+                // 发射一枚子弹
+                if (last_shoot_mode != SHOOT_MODE_SINGLE) {
+                    if (steering_motor->IsHolding()) {
+                        steering_motor->SetTarget(steering_motor->GetTarget() + 2 * PI / singleShotDivider, true);
+                    }
+                }
+                break;
+            case SHOOT_MODE_BURST:
+                steering_motor->SetTarget(steering_motor->GetTarget() + 2 * PI / singleShotDivider, true);
+                break;
+            case SHOOT_MODE_STOP:
+                // 停止发射
+                steering_motor->Hold(true);
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+void init_shoot() {
+    flywheel_left = new driver::Motor3508(can2, 0x201);
+    flywheel_right = new driver::Motor3508(can2, 0x202);
+    flywheel_left->SetTransmissionRatio(1);
+    flywheel_right->SetTransmissionRatio(1);
+
+    control::ConstrainedPID::PID_Init_t omega_pid_init = {
+        .kp = 150,
+        .ki = 0.03,
+        .kd = 1,
+        .max_out = 30000,
+        .max_iout = 10000,
+        .deadband = 0,                                          // 死区
+        .A = 3 * PI,                                            // 变速积分所能达到的最大值为A+B
+        .B = 2 * PI,                                            // 启动变速积分的死区
+        .output_filtering_coefficient = 0.1,                    // 输出滤波系数
+        .derivative_filtering_coefficient = 0,                  // 微分滤波系数
+        .mode = control::ConstrainedPID::Integral_Limit |       // 积分限幅
+                control::ConstrainedPID::OutputFilter |         // 输出滤波
+                control::ConstrainedPID::Trapezoid_Intergral |  // 梯形积分
+                control::ConstrainedPID::ChangingIntegralRate,  // 变速积分
+    };
+    flywheel_left->ReInitPID(omega_pid_init, driver::DjiMotorBase::OMEGA);
+    flywheel_right->ReInitPID(omega_pid_init, driver::DjiMotorBase::OMEGA);
+    flywheel_left->SetMode(driver::DjiMotorBase::OMEGA | driver::DjiMotorBase::INVERTED);
+    flywheel_right->SetMode(driver::DjiMotorBase::OMEGA);
+
+    steering_motor = new driver::Motor2006(can2, 0x203);
+
+    steering_motor->SetTransmissionRatio(19);
+    control::ConstrainedPID::PID_Init_t steering_theta_pid_init = {
+        .kp = 30,
+        .ki = 0,
+        .kd = 300,
+        .max_out = 4 * PI,
+        .max_iout = 0.25 * PI,
+        .deadband = 0,                                        // 死区
+        .A = 0,                                               // 变速积分所能达到的最大值为A+B
+        .B = 0,                                               // 启动变速积分的死区
+        .output_filtering_coefficient = 0.1,                  // 输出滤波系数
+        .derivative_filtering_coefficient = 0,                // 微分滤波系数
+        .mode = control::ConstrainedPID::Integral_Limit |     // 积分限幅
+                control::ConstrainedPID::OutputFilter |       // 输出滤波
+                control::ConstrainedPID::Trapezoid_Intergral  // 梯形积分
+    };
+    steering_motor->ReInitPID(steering_theta_pid_init, driver::DjiMotorBase::THETA);
+    control::ConstrainedPID::PID_Init_t steering_omega_pid_init = {
+        .kp = 800,
+        .ki = 0,
+        .kd = 5000,
+        .max_out = 10000,
+        .max_iout = 0,
+        .deadband = 0,                                           // 死区
+        .A = 2 * PI,                                             // 变速积分所能达到的最大值为A+B
+        .B = 1.5 * PI,                                           // 启动变速积分的死区
+        .output_filtering_coefficient = 0.1,                     // 输出滤波系数
+        .derivative_filtering_coefficient = 0,                   // 微分滤波系数
+        .mode = control::ConstrainedPID::Integral_Limit |        // 积分限幅
+                control::ConstrainedPID::OutputFilter |          // 输出滤波
+                control::ConstrainedPID::Trapezoid_Intergral |   // 梯形积分
+                control::ConstrainedPID::ChangingIntegralRate |  // 变速积分
+                control::ConstrainedPID::ErrorHandle,            // 错误处理
+
+    };
+    steering_motor->ReInitPID(steering_omega_pid_init, driver::DjiMotorBase::OMEGA);
+    steering_motor->SetMode(driver::DjiMotorBase::THETA | driver::DjiMotorBase::OMEGA);
+
+    steering_motor->RegisterErrorCallback(jam_callback, steering_motor);
+}
+void kill_shoot() {
+    steering_motor->Disable();
+    flywheel_left->Disable();
+    flywheel_right->Disable();
+}
